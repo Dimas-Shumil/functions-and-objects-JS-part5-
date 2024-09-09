040e6c8cadb093625ef7af5fbc09ee0b5105c831
Dmitry Shumilov <shum0@mail.ru>
2024-09-09 16:13:44 +0700

    Знакомство с функциями

---
+++ site/script/js.js
@@ -0,0 +1,53 @@
+
+// Получить кнопку:
+mybutton = document.getElementById("myBtn");
+
+// Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+function topFunction() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function passwordCheck(pass) {
+  if (pass === 'qwerty123') {
+      return true;
+  } else {
+      console.log('Неверный пароль! Повторите ввод');
+      return false;
+  }
+}
+
+passwordCheck("quesry") // false
+passwordCheck("'qwerty123'") // true
